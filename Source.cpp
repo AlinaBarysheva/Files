838daf99859b74ba49f20b063d5085ee5a0f1908
Alina <37037205+AlinaBarysheva@users.noreply.github.com>
2018-05-06 13:24:28 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cerrno>
+
+using namespace std;
+
+string file_name = "old_name.txt";
+
+int main() {
+	// Создание и открытие.
+	ofstream fout(file_name);
+	fout << "qwertyuiopasdfghjklzxcvbnm,\n\n\n\n\n\n\nfgsda26456345\n\n\n\n\tnbcvnvbcnvb";
+	// Закрытие файла.
+	fout.close();
+
+	system("pause");
+	cout << endl;
+
+	// Открытие файла для чтения.
+	ifstream fin(file_name);
+	int count = 0;
+	while (!fin.eof()) {
+		string str;
+		// Чтение строки из файла.
+		getline(fin, str);
+		cout << str << endl;
+
+		// Прибавляем количество символов строки к счётчику.
+		count += str.length();
+	}
+
+	system("pause");
+	cout << endl;
+
+	// Переименование.
+	//int result = rename(file_name.c_str(), "new_name.txt");
+	//file_name = "new_name.txt";
+	
+	system("pause");
+	cout << endl;
+
+	// Удаление файла.
+	remove(file_name.c_str());
+
+	system("pause");
+	return 0;
+}
